5a986d85c35d063cace1f53a4970af1188489fbd
Mikhail <mikushnerev@yandex.ru>
2022-02-11 15:52:06 +0300

    на ревью

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,69 @@
-# TODO:  Напишите свой вариант
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from posts.models import Follow, Group, Post
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from .permissions import IsOwnerOrReady
+from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
+                          PostSerializer)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReady]
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user
+        )
+
+class GroupVeiwSets(viewsets.ReadOnlyModelViewSet):
+
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class CommentViewSet(viewsets.ModelViewSet):
+
+    serializer_class = CommentSerializer
+    permission_classes = [IsOwnerOrReady]
+    # pagination_class = None
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post=get_object_or_404(
+                Post,
+                id=self.kwargs.get('post_id')
+            )
+        )
+
+    def get_queryset(self):
+        post = get_object_or_404(
+            Post,
+            id=self.kwargs.get('post_id')
+        )
+        return post.comments.all()
+
+class FollowViewSet(viewsets.ModelViewSet):
+
+    serializer_class = FollowSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['=user__username', '=following__username']
+    http_method_names = ('get', 'post',)
+
+    def perform_create(self, serializer):
+
+        serializer.save(
+            user=self.request.user,
+        )
+
+    def get_queryset(self):
+        return Follow.objects.filter(user=self.request.user)
--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+from .models import Post, Group, Comment, Follow
 # Register your models here.
+@admin.register(Post)
+class PostwAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'author',
+        'text',
+        'group',
+        'image'
+    )
+    empty_value_display = '-пусто-'
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug',
+        'description'
+    )
+    empty_value_display = '-пусто-'
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'following')
+    empty_value_display = '-пусто-'
